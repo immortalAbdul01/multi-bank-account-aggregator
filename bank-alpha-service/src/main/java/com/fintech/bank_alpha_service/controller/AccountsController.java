3fdeffe99ad03a33bb23d4ccb9d89f83ed0c952d
Immortal <ak2061435@gmail.com>
2026-03-15 02:55:18 +0530

    feat: implement bank-alpha-service with health check and accounts endpoint

---
+++ bank-alpha-service/src/main/java/com/fintech/bank_alpha_service/controller/AccountsController.java
@@ -0,0 +1,18 @@
+package com.fintech.bank_alpha_service.controller;
+
+import com.fintech.bank_alpha_service.model.Account;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class AccountsController {
+
+	@GetMapping("/accounts/{userId}")
+	public List<Account> getAccounts(@PathVariable String userId) {
+
+		return List.of(
+				new Account("A101", "Alpha Bank", 15000),
+				new Account("A102", "Alpha Bank", 8200));
+	}
+}
