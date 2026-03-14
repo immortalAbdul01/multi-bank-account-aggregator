0ebc811f2de206a9ab612ef386a05d95a1f8a5dc
Immortal <ak2061435@gmail.com>
2026-03-15 00:31:36 +0530

    initial spring boot project setup for bank alpha service

---
+++ bank-alpha-service/src/main/java/com/fintech/bank_alpha_service/BankAlphaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fintech.bank_alpha_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAlphaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAlphaServiceApplication.class, args);
+	}
+
+}
---
+++ bank-alpha-service/src/test/java/com/fintech/bank_alpha_service/BankAlphaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fintech.bank_alpha_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAlphaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
